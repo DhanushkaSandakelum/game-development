13665af6e1d2b5580141ed8364b5600e65bdaf8c
dhanushkasandakelum <dhanushkasandakelum711@gmail.com>
2022-07-24 02:31:47 +0530

    Cube ground added.

---
+++ 2-threejs-cubeGround/lights.js
@@ -0,0 +1,31 @@
+function ambient() {
+	const light = new THREE.AmbientLight(0xff0000, 0.4);
+	scene.add(light);
+}
+
+function directional() {
+	// White directional light at half intensity shining from the top.
+	const directionalLight = new THREE.DirectionalLight(0xff0000, 1);
+	directionalLight.position.set(20, 30, 30);
+	scene.add(directionalLight);
+}
+
+function spot() {
+	const spotLight = new THREE.SpotLight(0x00ff00, 0.8, 50, Math.PI / 4);
+	spotLight.position.set(0, 10, 0);
+	spotLight.castShadow = true; // default false
+	spotLight.shadow.mapSize.width = 1024;
+	spotLight.shadow.mapSize.height = 1024;
+
+	spotLight.shadow.camera.near = 500;
+	spotLight.shadow.camera.far = 4000;
+	spotLight.shadow.camera.fov = 30;
+	scene.add(spotLight);
+}
+
+function point() {
+	const light = new THREE.PointLight(0xffffff, 1);
+	light.position.set(0, 20, 0);
+	light.castShadow = true; // default false
+	scene.add(light);
+}
